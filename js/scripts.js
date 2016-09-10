889773786800be650f57c1829f17dd8255db1233
Nikolai Rezvoi <nikolai.rezvoi@gmail.com>
2016-09-10 15:24:44 +0300

    fullscreen menu, responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,5 @@
-var controller = new ScrollMagic.Controller();
-
-var tl1 = new TimelineMax();
-tl1.to("#nav-wrap", 1, { width: 145 })
-  .to("#works-fix", 1, { left: -10 }, 0)
-  .to("#nav", 1, { left: 40, transform: "translateX(0)" }, 0);
-/*.to("#nav-ab > .nav-line", 1, {width: "100%"}, 0);*/
-
-var scene1 = new ScrollMagic.Scene({ duration: "100%" })
-  .setTween(tl1)
-  .addTo(controller);
-
 //scrolltotop
-
-var flag = false;
-
-$(document).on("scroll", function () {
+$(document).on("scroll", function() {
   if ($(document).scrollTop() > $(window).height() * 0.3) {
     $("#scrollTop").css("opacity", 1);
     $("#scrollTop").css("visibility", "visible");
@@ -29,60 +14,21 @@ $(document).on("scroll", function () {
     $("#scrollTop").css("backgroundImage", "url(img/scrolltop.png)");
   }
 
-  //navbar
+  if ($(document).scrollTop() >= $(window).height() * 2 - 24 &&
+    $(document).scrollTop() < $(window).height() * 3 - 24) {
 
-  //first
-  if ($(document).scrollTop() >= $(window).height() - 110 &&
-    $(document).scrollTop() < $(window).height() * 2 - 110) {
+    $("#langs > a, #open-menu").css("color", "#000");
+    $(".bottom-line").css("backgroundColor", "#000");
 
-    if (!flag) {
-      $("#nav-wrap").fadeOut(100);
-      flag = true;
-      $("#nav-coll").fadeIn(100);
-    }
-
-    // $("header").css('background-color', "#4a3372");
-
-    TweenMax.to("header", 0.1, { backgroundColor: "#4a3372" });
-    $("#nav-icon").css("backgroundImage", "url(img/menu_icon.png)");
-    $("#langs > a").css("color", "#fff");
-    $("#langs > a.active").css("border-bottom-color", "#fff");
-
-    //second
-  } else if ($(document).scrollTop() >= $(window).height() * 2 - 110 &&
-  $(document).scrollTop() < $(window).height() * 3 - 110) {
-
-    if (!flag) {
-      $("#nav-wrap").fadeOut(100);
-      flag = true;
-      $("#nav-coll").fadeIn(100);
-    }
-
-    TweenMax.to("header", 0.1, { backgroundColor: "#ffef91" });
-    $("#nav-icon").css("backgroundImage", "url(img/menu_icon_black.png)");
-    $("#langs > a").css("color", "#000");
-    $("#langs > a.active").css("border-bottom-color", "#000");
-
-//third
-  } else if ($(document).scrollTop() >= $(window).height() * 3 - 110 )  {
-    $("#nav-icon").css("backgroundImage", "url(img/menu_icon.png)");
-    TweenMax.to("header", 0.1, { backgroundColor: "#c9326d" });
-    $("#langs > a").css("color", "#fff");
-    $("#langs > a.active").css("border-bottom-color", "#fff");
-//top
+    //third
   } else {
-    if (flag) {
-      $("#nav-wrap").fadeIn(100);
-      flag = false;
-      $("#nav-coll").fadeOut(100);
-    }
-
-    TweenMax.to("header", 0.1, { backgroundColor: "rgba(74,51,114,0)" });
+    $("#langs > a, #open-menu").css("color", "#fff");
+    $(".bottom-line").css("backgroundColor", "#fff")
   }
 
 });
 
-$("a").on('click', function (event) {
+$("a").on('click', function(event) {
 
   // Make sure this.hash has a value before overriding default behavior
   if (this.hash !== "") {
@@ -96,10 +42,30 @@ $("a").on('click', function (event) {
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
     $('html, body').animate({
       scrollTop: $(hash).offset().top
-    }, 1000, function () {
+    }, 1000, function() {
 
       // Add hash (#) to URL when done scrolling (default click behavior)
       window.location.hash = hash;
     });
   } // End if
+});
+
+$(".overlay-hugeinc").bind('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+  if ($(".overlay-hugeinc").hasClass('close')) {
+    $(".overlay-hugeinc").removeClass('close');
+    return;
+  }
+
+  return;
+});
+
+$("#open-menu").click(function() {
+  $(".overlay-hugeinc").addClass("open");
+  $("html,body").css("overflow", "hidden");
+});
+
+$(".close-menu").click(function() {
+  $(".overlay-hugeinc").removeClass("open");
+  $(".overlay-hugeinc").addClass("close");
+  $("html,body").css("overflow", "visible");
 });
